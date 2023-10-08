126a9f8897691c298264ee6680dd99e69124ff05
Aditi Kumari <aditisingh49893@gmail.com>
2023-10-08 19:44:19 +0530

    Setup of smtp using nodemailer has done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express=require('express');
 const bodyParser=require('body-parser');
 
 const {PORT}=require('./config/serverConfig');
+
+const {sendBasicEmail}=require('./services/email-service');
 const setUpAndStartServer=()=>{
     const app=express();
     app.use(bodyParser.json());
@@ -11,6 +13,13 @@ const setUpAndStartServer=()=>{
 
     app.listen(PORT,()=>{
 console.log(`server started at port ${PORT}`);
+
+   sendBasicEmail(
+    'support@admin.com',
+    'aditisingh27022002@gmail.com',
+    'This is a testing email',
+    'Congratulations you are selected'
+   );
     });
 }
 
